last approch to surface rotation was a huge mistake

src/specification/load_broadcast.py
@@ -6,6 +6,8 @@ import sys
 import importlib.util
 
 from src.surfaces import Surface
+from src.specifications import parse_toml
+
 
 class Broadcast:
     """Load up a test scenario"""
@@ -117,53 +119,3 @@ class Broadcast:
         surf_faxis = np.arange(numf) * self.fs / numt
 
         return taxis, faxis, surf_taxis, surf_faxis
-
-    def _setup_surface(self, toml_dict):
-        """Use scatter duration to bound surface integration"""
-
-        return surf, seed
-
-
-def load_broadcast(toml_file):
-    """load a surface scatter specification file"""
-    with open(toml_file, "rb") as f:
-        toml_dict = tomli.load(f)
-
-    # parse time frequency specifications
-    items = toml_dict['t_f'].items()
-    t_f = {}
-    for k, v in items:
-
-        if k in ['pulse']:
-            # these keys are not parsed by numexpr
-            t_f[k] = v
-        else:
-            t_f[k] = ne.evaluate(v)[()]
-
-    toml_dict['t_f'] = t_f
-
-    # parse soource receiver geometry specifications
-    items = toml_dict['geometry'].items()
-    toml_dict['geometry'] = {k: ne.evaluate(v)[()] for k, v in items}
-
-    # parse scatter surface specifications
-    items = toml_dict['surface'].items()
-    surface = {}
-    for k, v in items:
-
-        if k in ['type']:
-            # these keys are not parsed by numexpr
-            surface[k] = v
-        elif k == 'theta':
-            try:
-                surface[k] = ne.evaluate(v)[()]
-            except TypeError:
-                num_list = v.strip('[]').split(',')
-                num_list = np.array([ne.evaluate(n.strip()) for n in num_list])
-                surface[k] = num_list
-        else:
-            surface[k] = ne.evaluate(v)[()]
-
-    toml_dict['surface'] = surface
-
-    return toml_dict

src/surfaces/surface_realization.py
@@ -52,13 +52,6 @@ class Surface:
         self.rng = np.random.default_rng(self.seed)
 
 
-    def __call__(self):
-        """Generate and save surfaces"""
-        if self.surface_type in ['sine', 'flat']:
-            # these surfaces do not require precomputing
-            return
-
-
     def _surface_from_dict(self, sd):
         """deal with flat and sine special cases or generate a spectrum"""
         s_t = sd['type']
@@ -167,7 +160,8 @@ class Surface:
                       + self.y_a[None, :, None] * self.ky[None, None, :] \
                       + self.omega[None, None, :] * time
 
-            surf = self.spec_1D * np.exp(1j * phase) * np.sqrt(2)
+            spec_1d = self.spec_1d
+            surf = ne.evaluate("spec_1D * exp(1j * phase) * sqrt(2)")
             if derivative == 'x':
                 surf *= 1j * self.kx[..., :]
             elif derivative == 'y':