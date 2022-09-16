c652140042fab7844d21debf74bdccba27831299
Ned Richards <edwardlrichards@gmail.com>
2022-09-16 09:58:09 -0700

    working on surface shadow

--- src/surfaces/__init__.py
+++ src/surfaces/__init__.py
@@ -1,2 +1,3 @@
 from .spectrum import PM, directional_spectrum, e_delta, ldis_deepwater
-from .surface_realization import load_surface
+from .load_surface import load_surface
+from .surface_realization import Realization
--- src/surfaces/surface_realization.py
+++ src/surfaces/surface_realization.py
@@ -1,37 +1,143 @@
 import numpy as np
 import numexpr as ne
+from tempfile import mkdtemp
+import shutil
+from os import path
 
-class Eta:
-    """Specification of a single frozen surface realization"""
+class Realization:
+    """Class to create and manipulate surfaces on disk"""
 
-    def __init__(self, surface_position, gradient, hessian=None, x_a, y_a=None):
-        """surface postion is a 2xM or a 3xMxN array for a 2 or 3D geometry"""
-        self.surface_position = surface_position
-        self.x_a = x_a
-        if self.surface_position.shape[0] == 3 and y_a is None:
-            raise(ValueError("2D surface requires a y axis"))
-        self.y_a = y_a
+    def __init__(self, surface, include_hessian=False, chunksize=1e5):
+        """setup surface and save files"""
+        self.surface = surface
+        self.chunksize = chunksize
+        self.tmpdir = mkdtemp()
 
-    def distance_mask(self, z_src, r_rcr, d_max):
+        # generate a surface realization
+        realization = surface.gen_realization()
+
+        if realization is not None:
+            self.real_file = path.join(self.tmpdir, 'realization.dat')
+            real_mmap = np.memmap(self.real_file, dtype='float64',
+                                mode='w+', shape=realization.shape)
+            real_mmap = realization
+            real_mmap.flush()
+        else:
+            self.real_file = None
+
+        self.x_a = surface.x_a
+        self.y_a = surface.y_a
+
+        # setup file to store surfaces
+        if surface.y_a is None:
+            if include_hessian:
+                self.ndshape = (3, self.x_a.size)
+            else:
+                self.ndshape = (2, self.x_a.size)
+        else:
+            if include_hessian:
+                self.ndshape = (6, self.x_a.size, self.y_a.size)
+            else:
+                self.ndshape = (3, self.x_a.size, self.y_a.size)
+
+        self.include_hessian = include_hessian
+
+        self.eta_file = path.join(self.tmpdir, 'realization.dat')
+
+
+    def synthesize(self, time):
+        """Generate realization of surface"""
+
+        if self.real_file is not None:
+            real_mmap = np.memmap(self.real_file, dtype='float64', mode='r')
+            realization = np.array(real_mmap)
+        else:
+            realization = None
+
+        fp = np.memmap(self.eta_file, dtype='float64', mode='w+',
+                       shape=self.ndshape)
+        surf = self.surface
+
+        fp[0] = surf.surface_synthesis(realization, time=time, derivative=None)
+        fp[1] = surf.surface_synthesis(realization, time=time, derivative='x')
+
+        if surf.y_a is not None:
+            fp[2] = surf.surface_synthesis(realization, time=time, derivative='y')
+            save_ind = 3
+        else:
+            save_ind = 2
+
+        if self.include_hessian:
+            fp[save_ind] = surf.surface_synthesis(realization, time=time, derivative='xx')
+            if self.y_a is not None:
+                fp[save_ind + 1] = surf.surface_synthesis(realization, time=time, derivative='xy')
+                fp[save_ind + 2] = surf.surface_synthesis(realization, time=time, derivative='yy')
+
+        fp.flush()
+
+
+    def integral_mask(self, theta, to_shadow=False):
         """Return surface positions within a total range of d_max"""
-        z = self.surface_position
+        fp = np.memmap(self.eta_file, dtype='float64', mode='r',
+                       shape=self.ndshape)
+
+        # setup delay bound calculation
+        z_s = self.surface.z_src
+        z_r = self.surface.z_rcr
+        dr = self.surface.dr
+        d_max = self.surface.tau_max * self.surface.c
+
+        z = fp[0]
         x_r = r_rcr[0]
         z_r = r_rcr[-1]
         if self.y_a is None:
             x_a = self.x_a
             y_a = 0
+            x_r = dr
             y_r = 0
         else:
             x_a = self.x_a[:, None]
             y_a = self.y_a[None, :]
-            y_r = r_rcr[1]
+            x_r = dr * np.cos(theta)
+            y_r = dr * np.sin(theta)
 
-        d_src = ne.evaluate("sqrt(x_a ** 2 + y_a ** 2 + (z - z_src) ** 2)")
+        d_src = ne.evaluate("sqrt(x_a ** 2 + y_a ** 2 + (z - z_s) ** 2)")
         d_rcr = ne.evaluate("sqrt((x_r - x_a) ** 2 + (y_r - y_a) ** 2 + (z_r - z) ** 2)")
-        return (d_src + d_rcr) <= d_max
+        distance_mask = (d <= d_max)
+        if not to_shadow:
+            return distance_mask
+
+        # sort bearings by source and receiver distance
+        th_src = z_s / d_src
+        th_rcr = z_r / d_rcr
+        eta = z[distance_mask]
+        d_src = d_src[distance_mask]
+        d_rcr = d_rcr[distance_mask]
+
+        th_src = z_s / d_src
+        th_rcr = z_r / d_rcr
+        th_src = th_src[distance_mask]
+        th_rcr = th_rcr[distance_mask]
+
+        if self.y_a is not None:
+            dx = x_r
+            phi_src = ne.evaluate("(x_a * y_r - x_r * y_a) / (dr * sqrt(x_a ** 2 + y_a ** 2))")
+            phi_rcr = ne.evaluate("((x_r - x_a) * y_r - x_r * (y_r - y_a)) / (dr * sqrt((x_a - x_r) ** 2 + (y_a - y_r) ** 2))")
+            phi_src = phi_src[distance_mask]
+            phi_rcr = phi_rcr[distance_mask]
+
+        i_src = np.argsort(d_src)
+        i_rcr = np.argsort(d_rcr)
+
+
 
     def shadow_mask(self, z_src, r_rcr, distance_mask):
         """Create mask where either source or receiver ray is shadowed"""
+
+
         pass
 
-    def complete_integral(self, z_src, r_rcr, d_max):
+
+    def __del__(self):
+        """delete temporary directory"""
+        shutil.rmtree(self.tmpdir, ignore_errors=True)
