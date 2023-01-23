b3a48d8de71ee6a65d5932c7215a262091f95a1e
Ned Richards <edwardlrichards@gmail.com>
2023-01-22 22:02:55 -0800

    need to normalize spectra

--- kasea/surfaces/load_surface.py
+++ kasea/surfaces/load_surface.py
@@ -6,7 +6,9 @@ def load_surface(experiment):
     module = importlib.import_module('kasea.surfaces.' + surface_type)
     # case insentive search for surface class
     all_vals = dir(module)
-    class_i = [val.casefold() for val in all_vals].index(surface_type.casefold())
+    # Module is file name before underscore
+    class_name = (surface_type.split("_")[0]).casefold()
+    class_i = [val.casefold() for val in all_vals].index(class_name)
     surf_class = getattr(module, all_vals[class_i])
     curr_instance = surf_class(experiment)
     return curr_instance
